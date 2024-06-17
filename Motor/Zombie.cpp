9096ed9d11212eb7502273cc7f03f66b8b47da18
CATorresP <102825776+CATorresP@users.noreply.github.com>
2024-06-17 11:34:41 -0500

    Ubicación y visualización de zombies

--- Motor/Zombie.cpp
+++ Motor/Zombie.cpp
@@ -10,8 +10,11 @@ Zombie::~Zombie()
 
 void Zombie::init(float speed, glm::vec2 position)
 {
+	this->position = position;
+	color.set(80, 30, 80, 255);
 }
 
 void Zombie::update(vector<string>& levelData, vector<Human*>& humans, vector<Zombie*>& zombies)
 {
+
 }
